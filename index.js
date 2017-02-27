709f47e8452d57e5d0f64f447cfa53bde57d6ca8
Admin <vanpho02@gmail.com>
2017-02-27 19:47:27 +0700

    setup server

---
+++ index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Server started'));
+
+app.get('/', (req, res) => res.send('home'));
